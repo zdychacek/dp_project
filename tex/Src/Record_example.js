f17b0f0de0b5a8e383a34bbb07b8763ca3aad863
Ondrej Zdych <zdych.ondrej@gmail.com>
2014-03-25 19:55:04 +0100

    Tex upd

--- tex/Src/Record_example.js
+++ tex/Src/Record_example.js
@@ -1,6 +1,17 @@
 var recording =
 	new vxml.Record('Please record your information after the beep.');
 
+// NEBO
+
+var recording =
+	new vxml.Record({
+		prompt: 'Please record your information after the beep.',
+		maxTime: 60,
+		beep: true,
+		finalSilence: '2500ms',
+		type: 'audio/wav'
+	});
+
 var recordingState = vxml.State.create('getRecording', recording)
 	.addOnExitAction(function* (cf, state, event) {
 		var recordingDataBuffer = event.data;
